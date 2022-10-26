fec4bac74adf041846d40a6c792281ceda23efb5
Mya Willis <mwillis@pgh.yearup.org>
2022-10-26 15:12:53 -0400

    Create files

--- scripts/parsing.js
+++ scripts/parsing.js
@@ -1,1 +1,20 @@
-console.log('the script tag werks');
+function parseAndDisplayName (name) {
+    let space = name.indexOf(' ');
+    let firstName = name.substring(0, space);
+    let lastName = name.substring(space + 1);
+    console.log(firstName, lastName, name);
+}
+
+parseAndDisplayName('Robert DeNiro');
+
+
+
+// console.log('the script tag werks');
+// let fullName = "Kevin Ruse";
+// console.log(`the variable fullName is: ${fullName}`);
+// let space = fullName.indexOf(' ');
+// console.log(space); //?
+// let fName = fullName.substring(0, space);
+// console.log(fName);
+// let lastName = fullName.substring(space);
+// console.log(lastName);
